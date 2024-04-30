1a607cfb1bce2ad7f6cfb47784258d77bd864077
ritikb0618 <ritikb0618@gmail.com>
2024-05-01 00:45:17 +0530

    ritik1

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,19 @@
-tool_bar = ttk.Label(main_application)
-# tool_bar.pack(side=tk.TOP)
+def speech_to_text():
+#     recognizer = sr.Recognizer()
+#     with sr.Microphone() as source:
+#         recognizer.adjust_for_ambient_noise(source)
+#         print("Listening...")
+#         try:
+#             audio = recognizer.listen(source, timeout=5)  # Set a timeout to stop listening after 5 seconds of silence
+#             print("Stopped listening...")
+#             recognized_text = recognizer.recognize_google(audio)
+#             text_editor.insert(tk.END, recognized_text)
+#         except sr.WaitTimeoutError:
+#             print("No speech detected within the timeout period")
+#         except sr.UnknownValueError:
+#             print("Speech recognition could not understand audio")
+#         except sr.RequestError as e:
+#             print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-# # Bindings
-# tool_bar.bind("<Enter>", lambda event: tool_bar.style() = "normal")
-# tool_bar.bind("<Leave>", lambda event: tool_bar.style() = "hidden")
+
+# speech_to_text_btn.configure(command=speech_to_text)
